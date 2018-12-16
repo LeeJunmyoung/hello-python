1a6520990d4c5625e187934c2dbea14e1937b1fd
leejm1 <wnsaud5415@gamil.com>
2018-12-16 18:14:42 +0900

    함수 리턴& 매개변수 기본값

--- chap02.function/function.py
+++ chap02.function/function.py
@@ -4,12 +4,16 @@ def hello():
 
 hello()
 
+print("###########################")
+
 ## 기본 매개변수
 def dfVariableFunc(value,n):
     for i in range(n):
         print(value)
 
 dfVariableFunc("안녕하세요",10)
+
+print("###########################")
 
 ## 가변 매개변수
 def variableFunc(n, *values):
@@ -19,3 +23,44 @@ def variableFunc(n, *values):
         print()
 
 variableFunc(10,"안녕","하신","가요?")
+
+print("###########################")
+
+## 매개변수(디폴트 값)
+def valueDfFunc(value,n=2):
+    for i in range(n):
+        print(value)
+
+valueDfFunc("안녕")
+
+print("###########################")
+
+valueDfFunc("안녕",5)
+
+print("###########################")
+
+valueDfFunc(value="안녕",n=10)
+
+print("###########################")
+## 함수 리턴
+def sum_all(*values):
+    output = 0
+    for i in values:
+        output+=i
+    
+    return output
+
+print(sum_all(1,2,3,4,5,6,7,8,9,10))
+
+print("###########################")
+
+## 리스트 최대값 
+def custom_max(input_list):
+    output = input_list[0]
+    for element in input_list:
+        if output < element:
+            output=element
+
+    return output
+
+print(custom_max([100,22,55,33,88,500,200]))
