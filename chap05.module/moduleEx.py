0639b2fa2e52bd3ef788426c547ff89975938bcd
lee <wnsaud6016@naver.com>
2019-01-08 16:21:15 +0900

    python module 사용

---
+++ chap05.module/moduleEx.py
@@ -0,0 +1,20 @@
+import math as m 
+# import 모듈명 as 약칭
+from math import pi,sin 
+# from 모듈명 import 메서드 혹은 변수 
+print(m.pi)
+print(m.sin(10))
+
+print(pi)
+print(sin(10))
+
+import random
+
+print(random.random()*500)
+print(random.uniform(10,20))
+print(random.randrange(0,100,2))
+a= [1,2,3,4,5,6,7,8,9]
+print(a)
+
+random.shuffle(a)
+print(a)
