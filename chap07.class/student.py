fd15e65505c30e18d82f8d4708eba99b01d6405f
lee <wnsaud6016@naver.com>
2019-01-10 18:59:43 +0900

    접근지정자 및 클래스 변수 선언

--- chap07.class/student.py
+++ chap07.class/student.py
@@ -1,5 +1,11 @@
+
+
 class Student:
+    
+    __count=0
+
     def __init__(self,name,math,science,english):
+        Student.__count += 1
         self.name=name
         self.math=math
         self.science=science
@@ -15,8 +21,26 @@ class Student:
     def __str__(self):
         return "{}\t{}\t{}".format(self.name,self.getSum(),self.getAvarage())
 
+    def __eq__(self,value):
+        print("eq 함수가 호출되었습니다.")
+        return value == self.getAvarage()
 
+    @classmethod
+    def getCount(cls):
+        print(Student.__count)
+    
 
 a = Student("이준명",90,80,100)
+b = Student("이준명",90,80,98)
+c = Student("이준명",90,80,99)
 
 print(str(a))
+
+print(a==80)
+
+Student.getCount()
+print(Student.__count)
+
+## __num  =  private
+## _num   =  protected
+## num    =  public 
